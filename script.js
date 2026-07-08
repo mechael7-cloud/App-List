3b58acc62252743ee5c2b0f383945681a6e6f733
mechael7 <mechaelmandausanduo7@gmail.com>
2026-07-08 21:23:50 +0700

    change and add html css and javascript

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const buttonAdd = document.getElementById('button-add');
+const buttonplus = document.getElementById('button-plus');
 const selectMode = document.getElementById('select-mode');
+const deleteSelectMode = document.getElementById('delete-select-mode');
+
+
 buttonAdd.addEventListener('click', () => {
     selectMode.classList.toggle('hidden');
+})
+buttonplus.addEventListener('click', () => {
+    selectMode.classList.toggle('hidden');
+})
+deleteSelectMode.addEventListener('click', () => {
+    selectMode.classList.add('hidden');
 })
